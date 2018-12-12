600662a060258308a01e074a2b8aadebcabd9bdb
AddisonFPrice <addison.price@juiceanalytics.com>
2018-12-11 22:40:21 -0500

    adding properties to the post class

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from models import Post
 
-post1 = Post(title="The Rain King", content="He is a king.")
+post1 = Post(title="The Rain King", content="He is a king.", author="Addison Price")
 
 print(post1.title, post1.content)
 
--- models.py
+++ models.py
@@ -1,4 +1,9 @@
 class Post:
-    def __init__(self, title, content):
+    def __init__(self, blog_id, title, content, author, date, id):
+        self.blog_id = blog_id
         self.title = title
         self.content = content
+        self.author = author
+        self.date_created = date
+        self.id = id
+
