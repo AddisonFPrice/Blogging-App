18b9367107819e77b4ba015ec76e413ca7584326
AddisonFPrice <addison.price@juiceanalytics.com>
2018-12-17 15:51:38 -0500

    adding ALL CAPS to class (static) variables in the Database class

--- database.py
+++ database.py
@@ -3,21 +3,21 @@ import pymongo
 
 class Database:
     URI = "mongodb://127.0.0.1:27017"
-    database = None
+    DATABASE = None
 
     @staticmethod
     def connect(self):
         client = pymongo.MongoClient(Database.URI)
-        database = client['db1']
+        Database.DATABASE = client['db1']
 
     @staticmethod
     def find(collection, query):
-        return Database.database[collection].find(query)
+        return Database.DATABASE[collection].find(query)
 
     @staticmethod
     def find_one(collection, query):
-        return Database.database[collection].find_one(query)
+        return Database.DATABASE[collection].find_one(query)
 
     @staticmethod
     def insert(collection, data):
-        return Database.database[collection].insert(data)
+        return Database.DATABASE[collection].insert(data)
